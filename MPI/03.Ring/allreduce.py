bf71eac9dc445c2acaafa270e5e8e7aa09c8b2b0
Song-Hyunsung <song.hyunsung96@gmail.com>
2023-10-12 08:58:23 -0400

    impl buf spec for py

--- MPI/03.Ring/allreduce.py
+++ MPI/03.Ring/allreduce.py
@@ -9,7 +9,7 @@ size = comm_world.Get_size()
 rcv_buf = np.empty((), dtype=np.intc)
 snd_buf = np.array(my_rank, dtype=np.intc)
 
-comm_world.Allreduce(snd_buf, rcv_buf, MPI.SUM)
+comm_world.Allreduce((snd_buf, 1, MPI.INT), (rcv_buf, 1, MPI.INT), MPI.SUM)
 sum = rcv_buf
 
 print(f"PE{my_rank}:\tSum = {sum}")
