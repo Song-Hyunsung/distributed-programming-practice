0a9f2cd82a37b796262bab9cad1d7a0a538ede61
Song-Hyunsung <song.hyunsung96@gmail.com>
2023-12-06 19:00:18 -0500

    finish sample sort

--- MPI/06.SampleSort/sample-sort.py
+++ MPI/06.SampleSort/sample-sort.py
@@ -9,8 +9,8 @@ num_procs = comm_world.Get_size()
 # global_list_size = Total number of elements (evenly divisible by num_procs)
 # global_sample_size = Sample size (evenly divisible by num_procs)
 # num_procs = Number of processes
-global_list_size = 12
-sub_sample_size = 1
+global_list_size = 10000
+sub_sample_size = 16
 global_sample_size = sub_sample_size * num_procs
 global_sample = np.empty((global_sample_size), dtype=np.intc)
 splitter = np.empty((num_procs+1), dtype=np.intc)
@@ -23,7 +23,7 @@ ex_local_from_counts = np.zeros((num_procs), dtype=np.intc)
 
 # Generate list only in root process
 if(my_rank == 0):
-  list = np.array([random.randint(0, 100) for _ in range(global_list_size)], dtype=np.intc)
+  list = np.array([random.randint(0, 100000) for _ in range(global_list_size)], dtype=np.intc)
 
 # Every process here now receives sublist with n/p elements
 comm_world.Scatter(list, local_list, root=0)
@@ -52,8 +52,8 @@ if(my_rank == 0):
 # Broadcast splitter to all other processors
 comm_world.Bcast(splitter, root=0)
 
-if(my_rank == 0):
-  print(f"SPLITTER: {splitter}")
+# Sort each local list so that later alltoallv bucket distribution happens correctly
+local_list.sort()
 
 # For each local list, count how many elements are going to which processor
 for num in local_list:
@@ -73,17 +73,11 @@ for i in range(1, num_procs):
 
 # Call alltoallv with exclusive counts to place numbers onto correct buckets
 local_bucket_size = local_from_counts[num_procs-1] + ex_local_from_counts[num_procs-1]
-local_bucket_list = np.empty((local_bucket_size), dtype=np.intc)
-print(f"LOCAL LIST - {my_rank}: {local_list}")
-print(f"LOCAL TO COUNTS - {my_rank}: {local_to_counts}")
-print(f"LOCAL FROM COUNTS - {my_rank}: {local_from_counts}")
-print(f"EX LOCAL TO COUNTS - {my_rank}: {ex_local_to_counts}")
-print(f"EX LOCAL FROM COUNTS - {my_rank}: {ex_local_from_counts}")
+local_bucket_list = np.zeros((local_bucket_size), dtype=np.intc)
 comm_world.Alltoallv([local_list, local_to_counts, ex_local_to_counts, MPI.INT], [local_bucket_list, local_from_counts, ex_local_from_counts, MPI.INT])
 
 # Now that all numbers are in correct bucket, sort local buckets
 local_bucket_list.sort()
-print(f"LOCAL_BUCKET_LIST - {my_rank}: {local_bucket_list}")
 
 # Gather local bucket size to process 0
 global_bucket_counts = np.zeros((num_procs), dtype=np.intc)
@@ -96,15 +90,15 @@ if(my_rank == 0):
     ex_bucket_offsets[i] = ex_bucket_offsets[i-1] + global_bucket_counts[i-1]
 comm_world.Gatherv([local_bucket_list, local_bucket_size, MPI.INT], [list, global_bucket_counts, ex_bucket_offsets, MPI.INT], root=0)
 
-# if(my_rank == 0):
-#   sorted = True
-#   for i in range(len(list)-1):
-#     if list[i] > list[i+1]:
-#       print(f"List is not sorted properly")
-#       print(f"{list[i]} VS. {list[i+1]}")
-#       sorted = False
-#       break
-#   if(not sorted):
-#     print(f"{list}")
-#   else:
-#     print(f"List is sorted properly")
+if(my_rank == 0):
+  sorted = True
+  for i in range(len(list)-1):
+    if list[i] > list[i+1]:
+      print(f"{list[i]} VS. {list[i+1]}")
+      sorted = False
+      break
+  if(not sorted):
+    print(f"List is not sorted properly, errored out in above step.")
+  else:
+    print(f"List is sorted properly.")
+    print(f"{list}")
